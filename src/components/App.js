095ffa3fe6054cc95e885d512cfe2af34fe53947
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-18 14:43:37 +0000

    commented out the original code in app.js, used arrow functions to create the notes card

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,48 @@
+// import React from "react";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Note from "./Note";
+// import { notes } from "../notes";
+
+// function NoteCard(noteItem) {
+//   return (
+//     <Note
+//       key={noteItem.key}
+//       title={noteItem.title}
+//       content={noteItem.content}
+//     />
+//   );
+// }
+
+// function App(props) {
+//   return (
+//     <div>
+//       <Header />
+//       {notes.map(NoteCard)}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import { notes } from "../notes";
 
-function NoteCard(noteItem) {
-  return (
-    <Note
-      key={noteItem.key}
-      title={noteItem.title}
-      content={noteItem.content}
-    />
-  );
-}
-
 function App(props) {
   return (
     <div>
       <Header />
-      {notes.map(NoteCard)}
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
